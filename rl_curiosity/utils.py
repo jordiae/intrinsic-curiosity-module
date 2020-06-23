dd046b16749de4432b469153a3449d3643c3af99
Jordi Armengol Estapé <me@jordiae.com>
2020-06-24 01:20:28 +0200

    Changed normalize for older versions of torch

--- rl_curiosity/utils.py
+++ rl_curiosity/utils.py
@@ -70,7 +70,7 @@ def load_icm(args: argparse.Namespace, env: gym.Env) -> nn.Module:
 
 def transform(x: np.ndarray) -> np.ndarray:
     x = x.transpose((1, 2, 0))
-    return transforms.Compose([transforms.ToTensor(), transforms.Normalize(0.5, 0.5)])(x).numpy()
+    return transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])(x).numpy()
 
 
 @dataclass
